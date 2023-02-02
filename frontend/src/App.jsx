da1b753256ed6e53085e19fe7e028f91eb2e0b0a
settc <tewhittle@gmail.com>
2023-02-02 14:42:29 -0500

    Render info from json requested from weather app through Axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,24 +6,22 @@ const key = import.meta.env.VITE_API_KEY
 
 function App() {
 
-    const [loading, setLoading] = useState(true)
-    const [weather, setWeather] = useState([])
+  const [weather, setWeather] = useState([])
 
-    useEffect(() => {
-        Axios.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=Flagstaff`)
-             .then((response) => {
-                setWeather(response.data)
-                console.log(response.data)
-                setLoading(false)
-             }).catch(error => {
-                console.log(error)
-             })
-    }, [])
-  
+  useEffect(() => {
+      Axios.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=Flagstaff`)
+            .then((response) => {
+              setWeather(response.data)
+              console.log(response.data)              
+            }).catch(error => {
+              console.log(error)
+            })
+  }, [])
+
 
   return (
     <div className="App">
-     <Card />
+      <Card forecast={weather}/>
     </div>
   )
 }
--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,11 +1,13 @@
 import "./style.css"
+import { useState } from "react"
 
-function Card() {
+function Card(props) {
+
+    const location = props.forecast.location
+    const current = props.forecast.current
     
-    
-    
-    if (loading) {
-        return(
+    if (!location && !current) {
+        return (
             <div>
                 <p>Loading...</p>
             </div>
@@ -14,8 +16,11 @@ function Card() {
     
     return (
         <div>
-            
+           <p>{location.name}</p>
+           <p>{location.region}</p>
+           <p>{current.temp_f}</p>
         </div>
+        
     )
 }
 
