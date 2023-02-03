5a745dcb5dbb7f7ec067684c91fa29018b9c1cb5
settc <tewhittle@gmail.com>
2023-02-03 02:53:58 -0500

    Seperate hard coded query into 2 variables, one for current weather and one for forecast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,17 @@ import Axios from "axios"
 import { useEffect, useState } from "react"
 const key = import.meta.env.VITE_API_KEY
 
+
 function App() {
   
   const [weather, setWeather] = useState([])
   const [city, setCity] = useState("Columbus")
 
+  const currentQuery = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`
+  const forecastQuery = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=5`
+  
   useEffect(() => {
-      Axios.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`)
+      Axios.get(forecastQuery)
             .then((response) => {
               setWeather(response.data)
               console.log(response.data)              
